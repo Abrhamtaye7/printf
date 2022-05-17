38df3db942b7e4c53cdfbd43c4f48aa0ea1249bd
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-05-17 22:45:05 +0300

    task 3 u&o fix

--- print_any.c
+++ print_any.c
@@ -1,31 +1,10 @@
 #include "main.h"
 
-/**
- * helper - prints the numbers based on the type
- *
- * @num: The number to be printed.
- * @type: Base of the resulting number
- *
- * Return: It returns the number of characters to be printed.
- *
- */
-
-int helper(int num, int type)
-{
-	int ch_count = 0;
-
-	if (num / type)
-		ch_count += helper(num / type, type);
-
-	_putchar((num % type) + '0');
-
-	return (ch_count += 1);
-}
 
 /**
- * print_o - prints octal format of the number
+ * print_o - prints the unsigned octal format of the number.
  *
- * @ap: The argument parameter
+ * @ap: The argument parameter.
  *
  * Return: The number of characters printed by it.
  *
@@ -33,33 +12,50 @@ int helper(int num, int type)
 
 int print_o(va_list ap)
 {
-	int ch_count = 0;
-	unsigned int num = va_arg(ap, unsigned int);
+        unsigned int ch_count = 0, i, rem, div = 1;
 
-	ch_count += helper(num, 8);
+        unsigned int number = va_arg(ap, unsigned int);
 
-	return (ch_count);
+        for (i = 0; number / div > 7; i++, div *= 8)
+                ;
+        for (; div >= 1; number %= div, div /= 8, ch_count++)
+        {
+                rem = number / div;
+                _putchar(rem + '0');
+        }
+
+        return (ch_count);
 }
 
 
+
 /**
- * print_u - prints decimal format of the number
+ * print_u - prints the unsigned decimal format of the number.
  *
- * @ap: Argument parameter.
+ * @ap: The argument parameter.
  *
- * Return: Returns the number of characters to be printed
+ * Return: The number of characters printed by it.
  *
  */
 
 int print_u(va_list ap)
 {
-	int ch_count = 0;
-	unsigned int num = va_arg(ap, unsigned int);
+        unsigned int ch_count = 0, i, rem, div = 1;
 
-	ch_count += helper(num, 10);
+        unsigned int number = va_arg(ap, unsigned int);
 
-	return (ch_count);
+        for (i = 0; number / div > 9; i++, div *= 10)
+                ;
+        for (; div >= 1; number %= div, div /= 10, ch_count++)
+        {
+                rem = number / div;
+                _putchar(rem + '0');
+        }
+
+        return (ch_count);
 }
+
+
 
 /**
  * print_b - prints the binary format of the number.
